a14393aa378d9552255d5b8c03fe22d880445562
fuyanbin <been1986@gmail.com>
2019-08-11 23:56:23 +0800

    WebRTC相关逻辑移至App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ButtonBox from './Button';
 import VideoBox from './Video';
+import { WebRTC } from './connect';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.onConnected = this.onConnected.bind(this);
         this.onStart = this.onStart.bind(this);
         this.onConnect = this.onConnect.bind(this);
         this.onDisconnect = this.onDisconnect.bind(this);
@@ -13,13 +15,28 @@ class App extends React.Component {
         this.setVideoBoxRef = element => {
             this.videoBox = element;
         };
+
+        this.webRTC = new WebRTC();
+        this.webRTC.onConnect = this.onConnected;
+    }
+    
+    onConnected(stream) {
+        this.videoBox.addRemoteVideoStream(stream);
+    }
+
+    onClose() {
+        this.videoBox.onClose();
     }
 
     onStart() {
-        this.videoBox.start();
+        this.webRTC.signnaling();
+        this.webRTC.start().then(stream => {
+            this.videoBox.addLocalVideoStream(stream);
+        });
     }
 
-    onConnect(stream) {
+    onConnect() {
+        this.webRTC.connect();
     }
 
     onDisconnect() {
