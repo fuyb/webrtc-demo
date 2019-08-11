c59d9b268709e5a3ddbd19118f4a2e2e60793de7
fuyanbin <been1986@gmail.com>
2019-08-11 23:59:41 +0800

    加上一个RemoteVideo, WebRTC逻辑移至App.jsx, onClose() 置空video stream

--- src/Video.jsx
+++ src/Video.jsx
@@ -1,46 +1,67 @@
 import React from 'react';
-import { WebRTC } from './connect';
 
 class VideoBox extends React.Component {
     constructor(props) {
         super(props);
-        this.start = this.start.bind(this);
         this.addLocalVideoStream = this.addLocalVideoStream.bind(this);
-        this.removeLocalVideoStream = this.removeLocalVideoStream.bind(this);
+        this.addRemoteVideoStream = this.addRemoteVideoStream.bind(this);
+        this.onClose = this.onClose.bind(this);
 
-        this.video = null;
-        this.setVideoRef = element => {
-            this.video = element;
-            this.video.onloadedmetadata = e => {
-                this.video.play();
+        this.localVideo = null;
+        this.setLocalVideoRef = element => {
+            this.localVideo = element;
+            this.localVideo.onloadedmetadata = e => {
+                this.localVideo.play();
             }
         };
 
-        this.webRTC = new WebRTC();
+        this.remoteVideo = null;
+        this.setRemoteVideoRef = element => {
+            this.remoteVideo = element;
+            this.remoteVideo.onloadedmetadata = e => {
+                this.remoteVideo.play();
+            }
+        };
     }
 
     componentDidMount() {
     }
 
-    start() {
-        this.webRTC.start().then(stream => {
-            this.addLocalVideoStream(stream);
-        });
-    }
-
     addLocalVideoStream(stream) {
-        this.video.srcObject = stream;
+        this.localVideo.srcObject = stream;
     }
 
-    removeLocalVideoStream(stream) {
-        this.video.srcObject = null;
+    addRemoteVideoStream(stream) {
+        this.remoteVideo.srcObject = stream;
+    }
+
+    onClose() {
+        if (this.localVideo.srcObject !== null) {
+            let tracks = this.localVideo.stream.getTracks();
+             tracks.forEach(track => {
+                 track.stop();
+             });
+            this.localVideo.srcObject = null;
+        }
+
+        if (this.remoteVideo.srcObject !== null) {
+            let tracks = this.remoteVideo.stream.getTracks();
+             tracks.forEach(track => {
+                 track.stop();
+             });
+            this.remoteVideo.srcObject = null;
+        }
     }
 
     render () {
         return (
             <div className={"card"}>
-                <div id={"video-view"} className={"card_part card_part-two"}>
-                    <video ref={this.setVideoRef} controls>
+                <div className={"card_part card_part-two"}>
+                    <video ref={this.setLocalVideoRef} controls={true}>
+                    </video>
+                </div>
+                <div className={"card_part card_part-one"}>
+                    <video ref={this.setRemoteVideoRef} controls={true}>
                     </video>
                 </div>
             </div>
