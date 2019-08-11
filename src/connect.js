efadbf22f623ed681e6086001ecc79a9990be616
fuyanbin <been1986@gmail.com>
2019-08-12 00:17:08 +0800

    完成基本连接逻辑: 1. 通信两端先完成webrtc初始化，加好stream; 2. 发起端用 {ready: true} 通知对端开始连接; 3. 对端收到 {ready:true} 成为 Offer: createOffer() sdp --> setLocalDescription(sdp) -->send sdp; 4. 发起端 Receive Offer-sdb --> setRemoteDescription(Offer-sdp) --> --> createAnswer() sdp --> setLocalDescription(sdp) --> sedn sdp; 5. 对端 Receive Answer-sdp --> setRemoteDescription(Anwer-sdp) 6. 完成连接

--- src/connect.js
+++ src/connect.js
@@ -1,4 +1,5 @@
 import adapter from 'webrtc-adapter';
+import { SignnalingChannel } from './signnal';
 
 export function WebRTC() {
     const configuration = {
@@ -33,38 +34,99 @@ export function WebRTC() {
         return configuration;
     }
 
+    /* 建立signnal连接，用于交换数据 */
+    this.signnaling = () => {
+        /* channel */
+        const channel = window.location.search.match(/channel=(\d+)/)[1];
+        /* 本端ID */
+        const id = window.location.search.match(/id=(\d+)/)[1];
+        /* 对端ID */
+        const peer = window.location.search.match(/peer=(\d+)/)[1];
+        this.signnalingChannel = new SignnalingChannel(channel, id, peer);
+
+        this.signnalingChannel.onmessage = async (data) => {
+            try {
+                if (data.sdp) {
+                    console.log(data.sdp.type);
+                    switch (data.sdp.type) {
+                        case 'offer':
+                            await this.pc.setRemoteDescription(data.sdp);
+                            const stream =
+                                await navigator.mediaDevices.getUserMedia(constraints);
+                            stream.getTracks().forEach((track) =>
+                                this.pc.addTrack(track, stream));
+                            await this.pc.setLocalDescription(await this.pc.createAnswer());
+                            this.signnalingChannel.send({sdp: this.pc.localDescription});
+                            break;
+                        case 'answer':
+                            await this.pc.setRemoteDescription(data.sdp);
+                            break;
+                        default:
+                            console.log('Unsupported SDP type');
+                            break;
+                    }
+                } else if (data.candidate) {
+                    await this.pc.addIceCandidate(data.candidate);
+                } else if (data.ready) {
+                    /* 收到对端Ready询问 */
+                    this.createOffer();
+                    this.signnalingChannel.send({readyACK: true});
+                } else if (data.close) {
+                    /* 收到对端Close消息 */
+                    this.pc.close();
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        };
+    }
+
     this.pc = new window.RTCPeerConnection(this.getICEServices());
 
-    this.connectionHandler = function(option) {
-        /* candidate */
-        this.pc.onicecandidate = (candidate) => {
-            // send candidate
-        };
+    this.pc.onicecandidate = (event) => {
+        // send candidate
+        if (event.candidate) {
+            this.signnalingChannel.send({candidate: event.candidate});
+        }
+    };
 
-        this.pc.onconnectionstatechange = (event) => {
-            console.log(this.pc.connectionState);
-            switch(this.pc.connectionState) {
-                case 'connected':
-                    break;
-                case 'disconnected':
-                    break;
-                case 'failed':
-                    break;
-                case 'close':
-                    break;
-                default:
-                    break;
-            }
-        };
+    this.pc.onconnectionstatechange = (event) => {
+        console.log(this.pc.connectionState);
+        switch(this.pc.connectionState) {
+            case 'connected':
+                break;
+            case 'disconnected':
+                break;
+            case 'failed':
+                break;
+            case 'close':
+                if (this.onClose !== null) {
+                    this.onClose();
+                }
+                break;
+            default:
+                break;
+        }
+    };
 
-        this.pc.onnegotiationneeded = async () => {
-            console.log('onnegotiationneeded--->');
-            try {
-                await this.pc.setLocalDescription(await this.pc.createOffer(offerConfig));
-            } catch(error) {
-                console.log(error);
+    this.pc.onnegotiationneeded = async () => {
+        // 可以开始协商连接
+        console.log('onnegotiationneeded');
+    };
+
+    this.onConnect = null;
+    this.onClose = null;
+
+    // 对端视频流
+    this.pc.ontrack = (event) => {
+        console.log(event);
+        try {
+            if (this.onConnect !== null) {
+                this.onConnect(event.streams[0]);
             }
-        };
+        } catch (err) {
+            console.log(err);
+        }
     };
 
     this.start = async function() {
@@ -79,6 +141,20 @@ export function WebRTC() {
         } 
     };
 
+    this.createOffer = async function() {
+        try {
+            await this.pc.setLocalDescription(await this.pc.createOffer(offerConfig));
+            this.signnalingChannel.send({sdp: this.pc.localDescription});
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
     this.connect = async function() {
+        this.signnalingChannel.send({ready: true});
+    }
+
+    this.disconnect = async function() {
+        this.signnalingChannel.send({close: true});
     }
 }
